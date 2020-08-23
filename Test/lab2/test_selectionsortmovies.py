edd9d9e46a9d280b5f29c800ec4261aaa43e3999
Isaac David Bermudez Lara <i.bermudezl@uniandes.edu.co>
2020-08-23 12:31:18 -0500

    Fixed selection sort

--- Test/lab2/test_selectionsortmovies.py
+++ Test/lab2/test_selectionsortmovies.py
@@ -1,6 +1,6 @@
 import config
 import pytest
-from Sorting import shellsort as sort
+from Sorting import selectionsort as sort
 from DataStructures import listiterator as it
 from ADT import list as lt
 import helper
@@ -8,15 +8,15 @@ from test_arraylistmovies import load_arraylistmovies
 from test_singlelinkedlistmovies import load_linkedlistmovies
 
 
-def test_shellsort_movies_linkedlist(load_arraylistmovies):
+def test_selectionsort_movies_linkedlist(load_arraylistmovies):
     details, casting = load_arraylistmovies
 
-    sort.shellSort(details, helper.less)
-    sort.shellSort(casting, helper.less)
+    sort.selectionSort(details, helper.less)
+    sort.selectionSort(casting, helper.less)
 
 
-def test_shellsort_movies_array(load_linkedlistmovies):
+def test_selectionsort_movies_array(load_linkedlistmovies):
     details, casting = load_linkedlistmovies
 
-    sort.shellSort(details, helper.less)
-    sort.shellSort(casting, helper.less)
+    sort.selectionSort(details, helper.less)
+    sort.selectionSort(casting, helper.less)
